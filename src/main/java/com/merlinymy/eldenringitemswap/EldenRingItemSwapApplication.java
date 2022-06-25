edc9bb9335bd23b2b556523531471b1883fbefac
merlinymy <lordhorcrux@gmail.com>
2022-06-25 02:38:32 -0500

    initial commit

---
+++ src/main/java/com/merlinymy/eldenringitemswap/EldenRingItemSwapApplication.java
@@ -0,0 +1,13 @@
+package com.merlinymy.eldenringitemswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldenRingItemSwapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EldenRingItemSwapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merlinymy/eldenringitemswap/EldenRingItemSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlinymy.eldenringitemswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldenRingItemSwapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
